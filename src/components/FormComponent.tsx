61a4f8bacd096614aa5fc40f0e6e6a58fdabfdc6
root <root@DESKTOP-4GUT5LC.localdomain>
2023-07-25 09:01:51 +0530

    prod build 2

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -7,6 +7,7 @@ import { FormData , FormValidator } from '@/validations/formValidations'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { m } from 'framer-motion'
 import { itemScale, itemSlide } from '@/variants/textVariants'
+import LazyMotionComponent from './LazyMotionComponent'
 
 interface FormComponentProps {
   
@@ -42,6 +43,8 @@ const FormComponent: FC<FormComponentProps> = ({}) => {
   })
 
   return <form onSubmit={onSubmit} className='w-[30%] flex flex-col gap-2'>
+    <LazyMotionComponent>
+      
       
   {showSuccess && <p className='m-3 text-lg  text-green-400 font-bold animate-bounce duration-700 tracking-widest' >Form has been submitted</p>}
 
@@ -54,6 +57,7 @@ const FormComponent: FC<FormComponentProps> = ({}) => {
     <m.input variants={itemScale} initial='hidden' whileInView='visible' type="text" {...register('city')} className='input'  placeholder='Enter your city'/>
     <p className='m-2 tracking-wide text-base  font-semibold text-red-800'>{errors.city?.message}</p>
     <m.button variants={itemSlide} initial='hidden' whileInView='visible' className=' mx-auto border rounded-full bg-[#5c24ff] px-4 py-2 text-xl hover:bg-[#3a19a7] active:translate-y-2 transition-all duration-75 '>Submit</m.button>
+    </LazyMotionComponent>
     </form>
 }
 
